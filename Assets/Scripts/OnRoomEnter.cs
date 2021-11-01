daa258d42eb75f5d77ff7481b08bbf83c86cc5f6
T-Nugget <52225050+T-Nugget@users.noreply.github.com>
2021-11-01 16:52:39 -0400

    Added script for camera
    THIS IS WIP DONT MERGE TO MAIN.
    I can't access the cinemachine library.

---
+++ Assets/Scripts/OnRoomEnter.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnRoomEnter : MonoBehaviour
+{
+
+//THIS CODE DON'T WORK YET! ANGER!
+
+
+//    public GameObject mainVirtualCamera;
+//     public Collider newBoundingBox;
+//     public GameObject TutorialManager;
+
+//     private void OnTriggerEnter(Collider other)
+//     { 
+//         if (other.gameObject.CompareTag ("Player"))
+//         {
+//             //Change cinemachine confiner bounding volume property
+//             //Debug.Log("Transferring camera to new bounding box...");
+//             mainVirtualCamera.GetComponent<CinemachineConfiner>().m_BoundingVolume = newBoundingBox;
+//             if (TutorialManager != null)
+//             {
+//                 TutorialManager.GetComponent<SC_TutorialManager>().setCurrentRoom(this.gameObject);
+//             }
+//         }    
+//     }
+
+//     private void OnTriggerStay(Collider other)
+//     {
+//         if (other.gameObject.CompareTag("Player"))
+//         {
+//             //Change cinemachine confiner bounding volume property
+//             //Debug.Log("Transferring camera to new bounding box...");
+//             mainVirtualCamera.GetComponent<CinemachineConfiner>().m_BoundingVolume = newBoundingBox;
+//             if (TutorialManager != null)
+//             {
+//                 TutorialManager.GetComponent<SC_TutorialManager>().setCurrentRoom(this.gameObject);
+//             }
+//         } 
+//     }
+}
