8e27a48b56a53758b428ccdf9cec05731a293e1a
Eric O <ericdanielosorio@gmail.com>
2021-11-11 21:58:48 -0500

    Implement player reset to level start
    - Modified CheckpointHandler to now properly reset the player to level start when all lives are lost
    - Updated Checkpoint&LivesTesting prefab to include an example of the checkpoint prefab that has a distanced respawn and trigger

--- Assets/Scripts/CheckpointHandler.cs
+++ Assets/Scripts/CheckpointHandler.cs
@@ -5,24 +5,20 @@ using UnityEngine;
 public class CheckpointHandler : MonoBehaviour
 {
     private GameObject _player;
-    private Transform _levelStart
-    {
-        get => _levelStart;
-        set
-        {
-            _levelStart = _player.transform;
-        }
-    }
+    private Vector3 _levelStart;
     public Transform LastCheckpoint;
 
     private void Start()
     {
         _player = GameObject.FindGameObjectWithTag("Player");
+        _levelStart = _player.transform.position;
     }
 
     public void ResetToLevelStart()
     {
-        _player.transform.SetPositionAndRotation(_levelStart.transform.position, Quaternion.identity);
+        _player.GetComponent<CharacterController>().enabled = false;
+        _player.transform.SetPositionAndRotation(_levelStart, Quaternion.identity);
+        _player.GetComponent<CharacterController>().enabled = true;
     }
 
     public void ResetToLastCheckpoint()
