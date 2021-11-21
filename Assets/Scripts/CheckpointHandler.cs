fbc9f08d75047672ad0d49efe86e231c39b311cc
Eric O <ericdanielosorio@gmail.com>
2021-11-21 18:27:18 -0500

    Add checkpoint notification text
    - Added text that pops up for a settable duration of time to notify the player they've entered a checkpoint
    - Updated prefabs with changes

--- Assets/Scripts/CheckpointHandler.cs
+++ Assets/Scripts/CheckpointHandler.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CheckpointHandler : MonoBehaviour
 {
     private GameObject _player;
     private Vector3 _levelStart;
+
+    public float checkpointTextDuration = 3.0f;
+    public Text checkpointText;
     public Transform LastCheckpoint;
 
     private void Start()
@@ -26,5 +30,16 @@ public class CheckpointHandler : MonoBehaviour
         _player.GetComponent<CharacterController>().enabled = false;
         _player.transform.SetPositionAndRotation(LastCheckpoint.transform.position, Quaternion.identity);
         _player.GetComponent<CharacterController>().enabled = true;
+    }
+
+    public void PopUpCheckpointText()
+    {
+        checkpointText.enabled = true;
+        Invoke("DeactivateCheckpointText", checkpointTextDuration);
+    }
+
+    public void DeactivateCheckpointText()
+    {
+        checkpointText.enabled = false;
     }
 }
