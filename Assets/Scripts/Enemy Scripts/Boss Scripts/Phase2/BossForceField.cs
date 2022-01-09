b11e431d53cc25f9f7a9a805a825aa27269c9094
Eric O <ericdanielosorio@gmail.com>
2022-01-09 17:02:34 -0500

    Changed name of forcefieldScript

--- Assets/Scripts/Enemy Scripts/Boss Scripts/Phase2/BossForceField.cs
+++ Assets/Scripts/Enemy Scripts/Boss Scripts/Phase2/BossForceField.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class forcefieldScript : MonoBehaviour
+public class BossForceField : MonoBehaviour
 {
 
     public GameObject forcefield;
