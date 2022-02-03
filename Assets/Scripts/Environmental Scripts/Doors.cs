5f1dab226a9b55b703b1f0ba49523bd9faf85700
T-Nugget <52225050+T-Nugget@users.noreply.github.com>
2022-02-02 22:54:57 -0500

    Added an indicator for when youve picked up a key
    Added UI element that displays when a key is picked up.

--- Assets/Scripts/Environmental Scripts/Doors.cs
+++ Assets/Scripts/Environmental Scripts/Doors.cs
@@ -4,21 +4,47 @@ using UnityEngine;
 
 public class Doors : MonoBehaviour
 {
-    public GameObject doorKey;    
+    public GameObject doorKey;
+    public GameObject holder;  
+    public static bool UIkey = false;
     public GameObject door;
+    Transform objectToFollow;
+    public Vector3 offset;
+
+    void Start()
+    {
+        UIkey = false;
+    }
+
+    void Update()
+    {
+        if(UIkey == true)
+        {
+            objectToFollow = GameObject.Find("Player").transform;
+            transform.position = objectToFollow.position + offset;
+        }
+    }
  
 
     public void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Player")
+        if(other.gameObject.tag == "Player")  //If the player touches the key item in the game
+        {
+            holder.SetActive(false); //this makes the key in the overworld disapear. 
+            UIkey = true; //This makes the image of a key appear in the lower left of your screen.
+        }
+
+        if((other.gameObject.tag == "Door") && (UIkey == true))
         {
             doorKey.SetActive(false);
             door.transform.Rotate(new Vector3(0, 75, 0) );
+            UIkey = false;
 
         }
+
+
+
     }
-
-
 
 
     
