886719f9e4d5b7614a537dd501a4c8cca1a1e56e
Eric O <ericdanielosorio@gmail.com>
2022-02-13 18:11:29 -0500

    Improve Prototype Button
    - Made it to where PrototypeButton's pressable section goes inactive to visually demonstrate its pressed
    - Added WorldTriggers layer

--- Assets/Scripts/Environmental Scripts/Buttons.cs
+++ Assets/Scripts/Environmental Scripts/Buttons.cs
@@ -6,9 +6,12 @@ public class Buttons : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Movable")
-        {
-            Debug.Log($"{this.gameObject.name} Pressed!");
-        }
+        Debug.Log($"{this.gameObject.name} Pressed!");
+        this.gameObject.transform.Find("ButtonPressable").gameObject.SetActive(false);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log($"{this.gameObject.name} Released!");
+        this.gameObject.transform.Find("ButtonPressable").gameObject.SetActive(true);
     }
 }
