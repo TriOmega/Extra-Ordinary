55cd45c0365ff70c0fd42d8e12319741e23882ce
T-Nugget <52225050+T-Nugget@users.noreply.github.com>
2022-03-20 03:16:30 -0400

    Adjustment to scene transitions

--- Assets/Scripts/Enemy Scripts/Boss Scripts/Phase2/BossSinkIntoGround.cs
+++ Assets/Scripts/Enemy Scripts/Boss Scripts/Phase2/BossSinkIntoGround.cs
@@ -43,7 +43,7 @@ public class BossSinkIntoGround : StateMachineBehaviour
 
         if (timeRemaining < 2)
         {
-            SceneManager.LoadScene("Ending");
+            SceneManager.LoadScene("AustinScene");
         }
     }
 
