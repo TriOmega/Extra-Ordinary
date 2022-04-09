8b8491d6c992b6714e8ea4f91ab443ab744ced61
Eric O <ericdanielosorio@gmail.com>
2022-04-09 15:38:11 -0400

    Created code base for cylinder scale extension

--- Assets/Scripts/Environmental Scripts/KeyMirror.cs
+++ Assets/Scripts/Environmental Scripts/KeyMirror.cs
@@ -6,15 +6,13 @@ public class KeyMirror : MonoBehaviour
 {
     [SerializeField, Tooltip("Put reference to a door's key that is hidden out of bounds here.")]
     private GameObject key;
-    [SerializeField]
-    private bool isKeyMirror = false;
 
     [SerializeField]
     private List<ReflectingMirror> reflectingMirrors = new List<ReflectingMirror>();
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Flashlight") && isKeyMirror)
+        if (other.transform.parent.GetComponent<ReflectingMirror>() == reflectingMirrors[0])
         {
             Destroy(this.gameObject.transform.GetChild(0).gameObject);
             key.transform.position = transform.position + transform.forward;
--- Assets/Scripts/Environmental Scripts/ReflectingMirror.cs
+++ Assets/Scripts/Environmental Scripts/ReflectingMirror.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class ReflectingMirror : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //[SerializeField, Tooltip("Put KeyMirror that this objec"]
+    //private KeyMirror keyMirrorReference;
+    [SerializeField]
+    private Transform reflectionBeamStartPoint;
+    [SerializeField]
+    private Transform reflectionBeamEndPoint;
+    [SerializeField]
+    private Transform reflectionBeam;
+
+    private void Update()
     {
-        
+        float distance;
+        distance = Vector3.Distance(reflectionBeamStartPoint.position, reflectionBeamEndPoint.position);
+        reflectionBeam.localScale = new Vector3(reflectionBeam.localScale.x, distance/2f, reflectionBeam.localScale.z);
+        Vector3 midPoint = (reflectionBeamStartPoint.position + reflectionBeamEndPoint.position) / 2f;
+        reflectionBeam.position = midPoint;
+        Vector3 rotationDirection = (reflectionBeamEndPoint.position - reflectionBeamStartPoint.position);
+        reflectionBeam.up = rotationDirection;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Flashlight"))
+        {
+
+        }
     }
 }
