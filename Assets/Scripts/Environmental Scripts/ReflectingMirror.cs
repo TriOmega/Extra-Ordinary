d310247ddfa3ca65281cc08827cb28a7b6236f44
Eric O <ericdanielosorio@gmail.com>
2022-04-09 18:22:49 -0400

    Add aimable reflection beam
    - Added LineRenderer to create reflecting light beam effect
    - Light beam activates currently on just getting hit by flashlight

--- Assets/Scripts/Environmental Scripts/ReflectingMirror.cs
+++ Assets/Scripts/Environmental Scripts/ReflectingMirror.cs
@@ -6,29 +6,45 @@ public class ReflectingMirror : MonoBehaviour
 {
     //[SerializeField, Tooltip("Put KeyMirror that this objec"]
     //private KeyMirror keyMirrorReference;
+    [SerializeField, Tooltip("Place reflection beam pivot here. Use pivot object to aim reflection beam.")]
+    private Transform reflectionBeamPivot;
     [SerializeField]
-    private Transform reflectionBeamStartPoint;
-    [SerializeField]
-    private Transform reflectionBeamEndPoint;
-    [SerializeField]
-    private Transform reflectionBeam;
+    private float reflectionBeamEndSurplus = .75f;
+    private RaycastHit hit;
+    private LineRenderer lineRenderer;
+
+    private void Start()
+    {
+        reflectionBeamPivot.gameObject.SetActive(false);
+        lineRenderer = GetComponent<LineRenderer>();
+    }
 
     private void Update()
     {
-        float distance;
-        distance = Vector3.Distance(reflectionBeamStartPoint.position, reflectionBeamEndPoint.position);
-        reflectionBeam.localScale = new Vector3(reflectionBeam.localScale.x, distance/2f, reflectionBeam.localScale.z);
-        Vector3 midPoint = (reflectionBeamStartPoint.position + reflectionBeamEndPoint.position) / 2f;
-        reflectionBeam.position = midPoint;
-        Vector3 rotationDirection = (reflectionBeamEndPoint.position - reflectionBeamStartPoint.position);
-        reflectionBeam.up = rotationDirection;
+        lineRenderer.SetPosition(0, reflectionBeamPivot.position);
+        if (Physics.Raycast(reflectionBeamPivot.position, reflectionBeamPivot.up, out hit, Mathf.Infinity))
+        {
+            if (hit.collider)
+            {
+                lineRenderer.SetPosition(1, hit.point + (reflectionBeamPivot.up * reflectionBeamEndSurplus));
+            }
+        }
+        else lineRenderer.SetPosition(1, reflectionBeamPivot.up * 50);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Flashlight"))
         {
+            lineRenderer.enabled = true;
+        }
+    }
 
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Flashlight"))
+        {
+            lineRenderer.enabled = false;
         }
     }
 }
