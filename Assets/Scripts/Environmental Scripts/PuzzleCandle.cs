4489e5cc61792414f1e51942a2012c5fb31230fa
Eric O <ericdanielosorio@gmail.com>
2022-04-11 18:52:52 -0400

    Add candle puzzle interworking
    - Candle puzzle interworking are functional just missing the decreasing candle flicker volume and the door opening code

---
+++ Assets/Scripts/Environmental Scripts/PuzzleCandle.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PuzzleCandle : MonoBehaviour
+{
+    public event EventHandler onCandleLit;
+
+    [SerializeField, Tooltip("Place candle lights object here.")]
+    private GameObject candleLights;
+    public bool isCandleLit = false;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Flashlight"))
+        {
+            if (!isCandleLit)
+            {
+                isCandleLit = true;
+                candleLights.SetActive(true);
+                onCandleLit?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+
+    public void BlowOutCandle()
+    {
+        isCandleLit = false;
+        candleLights.SetActive(false);
+    }
+}
