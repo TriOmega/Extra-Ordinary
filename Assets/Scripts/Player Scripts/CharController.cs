eb9f3d286df574e7311d920d6dbdb649847d05aa
Eric O <ericdanielosorio@gmail.com>
2022-04-13 02:02:58 -0400

    Add manual scene loads and fix player spawn in HH

--- Assets/Scripts/Player Scripts/CharController.cs
+++ Assets/Scripts/Player Scripts/CharController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CharController : MonoBehaviour
 {
@@ -39,6 +40,27 @@ public class CharController : MonoBehaviour
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SceneManager.LoadScene("0_MainMenuScene");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SceneManager.LoadScene("1_BathroomScene");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SceneManager.LoadScene("2_MuseumScene");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            SceneManager.LoadScene("3_HauntedHouseScene");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            SceneManager.LoadScene("BossScene");
+        }
+
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
         if (isGrounded && velocity.y < 0)
