e21003c5d9bfa116bec130ee2e49fc53818649bf
T-Nugget <52225050+T-Nugget@users.noreply.github.com>
2022-04-22 16:20:17 -0400

    final boss phase 2 now exists

---
+++ Assets/Scripts/Enemy Scripts/FinalBossScripts/SadWalk.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SadWalk : StateMachineBehaviour
+{
+    GameObject player;
+    
+    private Transform playerPosition;
+    private Rigidbody BossCurrentPosition;
+    float distanceFromPlayer = 1f;
+    float timeRemaining;
+
+
+
+
+    //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        playerPosition = GameObject.FindGameObjectWithTag("Player").transform;
+        BossCurrentPosition = animator.GetComponent<Rigidbody>();
+        animator.GetComponent<UnityEngine.AI.NavMeshAgent>().speed = 1.5f;  //Slow Mateo down while he's doing the sad walk. 
+        timeRemaining = 10f; 
+    }
+
+    //OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+        animator.GetComponent<UnityEngine.AI.NavMeshAgent>().destination = playerPosition.position;  //Follow the player
+
+        timeRemaining -= Time.deltaTime;
+        if (timeRemaining <= 5)
+        {
+            animator.SetTrigger("Transition4");
+            timeRemaining = 10f;
+        }
+
+    }
+
+    //OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.GetComponent<UnityEngine.AI.NavMeshAgent>().speed = 3.5f;
+        animator.ResetTrigger("Transition4");
+    }
+}
--- Assets/Scripts/Enemy Scripts/FinalBossScripts/StompingAttack.cs
+++ Assets/Scripts/Enemy Scripts/FinalBossScripts/StompingAttack.cs
@@ -5,40 +5,35 @@ using UnityEngine;
 public class StompingAttack : StateMachineBehaviour
 {
 
-    GameObject player;
-    private Transform playerPosition;
-    private Rigidbody BossCurrentPosition;
-    float distanceFromPlayer = 1f;
-
-
+    GameObject foot;
+    public GameObject shockwave;
+    bool isDone;
 
 
     //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        playerPosition = GameObject.FindGameObjectWithTag("Player").transform;
-        BossCurrentPosition = animator.GetComponent<Rigidbody>();
-        animator.GetComponent<UnityEngine.AI.NavMeshAgent>().speed = 1.5f;  //Slow Mateo down while he's stomping. 
+        foot = GameObject.Find("QuickRigCharacter_RightToeBase"); 
     }
 
     //OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        Vector3 shockwaveLocation = new Vector3(foot.transform.position.x, foot.transform.position.y, foot.transform.position.z);
 
-        animator.GetComponent<UnityEngine.AI.NavMeshAgent>().destination = playerPosition.position;  //Follow the player
-
-
-        if(Vector3.Distance(BossCurrentPosition.position, playerPosition.position) < distanceFromPlayer)   //Punch the player if he gets close enough.
+        if (!isDone)
         {
-            //animator.SetTrigger("ActivatePunchingAttack");
+            Instantiate (shockwave, shockwaveLocation, foot.transform.rotation);
+            isDone = true;
         }
+
 
     }
 
     //OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.GetComponent<UnityEngine.AI.NavMeshAgent>().speed = 3.5f;
+
         //animator.ResetTrigger("ActivatePunchingAttack");
     }
 
